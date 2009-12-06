c074667941b4869844ddd583feead6c1e06cc4b6
root <root@comedian.(none)>
2009-12-06 05:59:14 -0600

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# /etc/puppet/modules/puppetmaster/manifests/init.pp
+# FIXME: Totally broken now that we are on modules
+# TODO: have git send patch notes to sysadmins so we know what changed and when. Maybe send to RT queue instead?
+
+class puppetmaster {
+	exec { "git init && git add * && git commit -m 'Initial Commit'":
+		cwd	=> "/etc/puppet/manifests",
+		path	=> "/bin:/usr/bin",
+		unless	=> "test -e /etc/puppet/manifests/.git",
+	}
+	exec { "git add *; git commit --message='Autocommit'":
+		cwd	=> "/etc/puppet/manifests",
+		path	=> "/bin:/usr/bin:/usr/local/bin",
+	}
+}
+
