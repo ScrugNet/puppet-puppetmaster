925aace9a2714450ed2c22574eff357038c9865f
Josh Matthews <josh@redriverit.net>
2009-12-16 02:21:06 -0600

    Commented out dangerous code; not quite ready to delete it (may just be sentiment for a bad idea)

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,14 +3,14 @@
 # TODO: have git send patch notes to sysadmins so we know what changed and when. Maybe send to RT queue instead?
 
 class puppetmaster {
-	exec { "git init && git add * && git commit -m 'Initial Commit'":
-		cwd	=> "/etc/puppet/manifests",
-		path	=> "/bin:/usr/bin",
-		unless	=> "test -e /etc/puppet/manifests/.git",
-	}
-	exec { "git add *; git commit --message='Autocommit'":
-		cwd	=> "/etc/puppet/manifests",
-		path	=> "/bin:/usr/bin:/usr/local/bin",
-	}
+#	exec { "git init && git add * && git commit -m 'Initial Commit'":
+#		cwd	=> "/etc/puppet/manifests",
+#		path	=> "/bin:/usr/bin",
+#		unless	=> "test -e /etc/puppet/manifests/.git",
+#	}
+#	exec { "git add *; git commit --message='Autocommit'":
+#		cwd	=> "/etc/puppet/manifests",
+#		path	=> "/bin:/usr/bin:/usr/local/bin",
+#	}
 }
 
